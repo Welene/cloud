4572964d7aa8cc99d0f434d41f202753d68ae444
Welene <helenetheo@live.no>
2025-10-02 19:37:06 +0200

    Moved header, footer & btn-comp from App to Router  with Layout/Outlet

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,13 @@
 import React, { useState } from 'react';
 import Router from './router/Router';
 import { PostsContext } from './context/PostsContext';
-import NewPostBtn from './components/NewPostButton/NewPostBtn';
 
 function App() {
 	const [posts, setPosts] = useState([]);
 
 	return (
 		<PostsContext.Provider value={{ posts, setPosts }}>
-			<div className="app">
-				<header className="app-header">
-					<h1 className="app-header__title">QuickPost</h1>
-				</header>
-
-				<main className="app-main">
-					<Router />
-				</main>
-
-				<footer className="app-footer">
-					<p className="app-footer__text">© 2025 Helene Theodorsen</p>
-				</footer>
-			</div>
+			<Router />
 		</PostsContext.Provider>
 	);
 }
--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,20 +1,55 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import React from 'react';
+import {
+	createBrowserRouter,
+	RouterProvider,
+	Outlet,
+	useNavigate,
+} from 'react-router-dom'; // had to make a layout in the end, had accidentally put header and footer outside of footer: this was the fix, so now I can click the header and navigate
 import Home from '../pages/HomePage/Home';
 import UserPosts from '../pages/UserPostsPage/UserPosts';
 import NewPostPage from '../pages/NewPostPage/NewPostPage';
 import Login from '../pages/LoginPage/Login';
 import Register from '../pages/RegisterPage/Register';
-// import Logout from '../pages/LogoutPage/Logout;'
+import NewPostBtn from '../components/NewPostButton/NewPostBtn';
 
-const Router = ({ posts, setPost }) => {
-	// posts (old) & addPosts (new) props have to PASS THROUGH router to jump down to Home (all the way from NewPostPage)!
+const Layout = () => {
+	const navigate = useNavigate();
+
+	const handleHeaderClick = () => {
+		navigate('/');
+	};
+
+	return (
+		<div className="app">
+			<header className="app-header">
+				<h1 className="app-header__title" onClick={handleHeaderClick}>
+					QuickPost
+				</h1>
+			</header>
+			<main className="app-main">
+				<Outlet /> {/* content of the page here */}
+			</main>
+			<footer className="app-footer">
+				<p className="app-footer__text">© 2025 Helene Theodorsen</p>
+			</footer>
+			<NewPostBtn />
+		</div>
+	);
+};
+
+const Router = () => {
 	const router = createBrowserRouter([
-		{ path: '/', element: <Home /> },
-		{ path: '/user/:userId', element: <UserPosts /> },
-		{ path: '/login', element: <Login /> },
-		{ path: '/register', element: <Register /> },
-		{ path: '/newpost', element: <NewPostPage /> },
-		// {path: '/logout', element: <Logout/>},
+		{
+			path: '/',
+			element: <Layout />, // the layout will wrap all the different pages
+			children: [
+				{ path: '/', element: <Home /> },
+				{ path: '/user/:userId', element: <UserPosts /> },
+				{ path: '/login', element: <Login /> },
+				{ path: '/register', element: <Register /> },
+				{ path: '/newpost', element: <NewPostPage /> },
+			],
+		},
 	]);
 
 	return <RouterProvider router={router} />;
